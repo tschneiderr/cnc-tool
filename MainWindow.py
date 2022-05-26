ad14a8f334eaf514848d292b07edc571f0217ceb
Thomas Schneider <ts97@protonmail.com>
2022-05-26 20:22:31 +0200

    Add tabs and folder mode

--- MainWindow.py
+++ MainWindow.py
@@ -18,8 +18,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QGroupBox,
     QLabel, QLineEdit, QMainWindow, QMenuBar,
     QPlainTextEdit, QPushButton, QSizePolicy, QSpacerItem,
-    QSpinBox, QStatusBar, QToolButton, QVBoxLayout,
-    QWidget)
+    QSpinBox, QStatusBar, QTabWidget, QToolButton,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -30,9 +30,15 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab_file_mode = QWidget()
+        self.tab_file_mode.setObjectName(u"tab_file_mode")
+        self.verticalLayout_2 = QVBoxLayout(self.tab_file_mode)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
-        self.pte_file_preview = QPlainTextEdit(self.centralwidget)
+        self.pte_file_preview = QPlainTextEdit(self.tab_file_mode)
         self.pte_file_preview.setObjectName(u"pte_file_preview")
         font = QFont()
         font.setFamilies([u"Cascadia Mono Light"])
@@ -47,7 +53,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addItem(self.vs_functions_2, 5, 1, 1, 2)
 
-        self.pb_save_file = QPushButton(self.centralwidget)
+        self.pb_save_file = QPushButton(self.tab_file_mode)
         self.pb_save_file.setObjectName(u"pb_save_file")
         font1 = QFont()
         font1.setBold(True)
@@ -55,13 +61,13 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.pb_save_file, 3, 1, 1, 2)
 
-        self.le_file_path = QLineEdit(self.centralwidget)
+        self.le_file_path = QLineEdit(self.tab_file_mode)
         self.le_file_path.setObjectName(u"le_file_path")
         self.le_file_path.setReadOnly(True)
 
         self.gridLayout.addWidget(self.le_file_path, 0, 0, 1, 1)
 
-        self.tb_reload_file = QToolButton(self.centralwidget)
+        self.tb_reload_file = QToolButton(self.tab_file_mode)
         self.tb_reload_file.setObjectName(u"tb_reload_file")
         font2 = QFont()
         font2.setPointSize(10)
@@ -69,13 +75,13 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.tb_reload_file, 0, 1, 1, 1)
 
-        self.pb_open_file = QPushButton(self.centralwidget)
+        self.pb_open_file = QPushButton(self.tab_file_mode)
         self.pb_open_file.setObjectName(u"pb_open_file")
         self.pb_open_file.setFont(font1)
 
         self.gridLayout.addWidget(self.pb_open_file, 0, 2, 1, 1)
 
-        self.gb_functions = QGroupBox(self.centralwidget)
+        self.gb_functions = QGroupBox(self.tab_file_mode)
         self.gb_functions.setObjectName(u"gb_functions")
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
         sizePolicy.setHorizontalStretch(0)
@@ -202,14 +208,179 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addItem(self.vs_functions_1, 2, 1, 1, 2)
 
-        self.pb_save_file_as = QPushButton(self.centralwidget)
+        self.pb_save_file_as = QPushButton(self.tab_file_mode)
         self.pb_save_file_as.setObjectName(u"pb_save_file_as")
         self.pb_save_file_as.setFont(font1)
 
         self.gridLayout.addWidget(self.pb_save_file_as, 4, 1, 1, 2)
 
 
-        self.verticalLayout.addLayout(self.gridLayout)
+        self.verticalLayout_2.addLayout(self.gridLayout)
+
+        self.tabWidget.addTab(self.tab_file_mode, "")
+        self.tab_folder_mode = QWidget()
+        self.tab_folder_mode.setObjectName(u"tab_folder_mode")
+        self.verticalLayout_3 = QVBoxLayout(self.tab_folder_mode)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.gridLayout_3 = QGridLayout()
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.le_folder_path = QLineEdit(self.tab_folder_mode)
+        self.le_folder_path.setObjectName(u"le_folder_path")
+        self.le_folder_path.setReadOnly(True)
+
+        self.gridLayout_3.addWidget(self.le_folder_path, 0, 0, 1, 1)
+
+        self.tb_reload_folder = QToolButton(self.tab_folder_mode)
+        self.tb_reload_folder.setObjectName(u"tb_reload_folder")
+        self.tb_reload_folder.setFont(font2)
+
+        self.gridLayout_3.addWidget(self.tb_reload_folder, 0, 1, 1, 1)
+
+        self.pb_open_folder = QPushButton(self.tab_folder_mode)
+        self.pb_open_folder.setObjectName(u"pb_open_folder")
+        self.pb_open_folder.setFont(font1)
+
+        self.gridLayout_3.addWidget(self.pb_open_folder, 0, 2, 1, 1)
+
+        self.gb_functions_2 = QGroupBox(self.tab_folder_mode)
+        self.gb_functions_2.setObjectName(u"gb_functions_2")
+        sizePolicy.setHeightForWidth(self.gb_functions_2.sizePolicy().hasHeightForWidth())
+        self.gb_functions_2.setSizePolicy(sizePolicy)
+        self.gridLayout_4 = QGridLayout(self.gb_functions_2)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.ln_functions_4 = QFrame(self.gb_functions_2)
+        self.ln_functions_4.setObjectName(u"ln_functions_4")
+        self.ln_functions_4.setFrameShadow(QFrame.Plain)
+        self.ln_functions_4.setFrameShape(QFrame.HLine)
+
+        self.gridLayout_4.addWidget(self.ln_functions_4, 9, 0, 1, 2)
+
+        self.sb_number_lines_start_2 = QSpinBox(self.gb_functions_2)
+        self.sb_number_lines_start_2.setObjectName(u"sb_number_lines_start_2")
+        self.sb_number_lines_start_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.sb_number_lines_start_2.setMaximum(100000)
+        self.sb_number_lines_start_2.setSingleStep(100)
+        self.sb_number_lines_start_2.setValue(1000)
+
+        self.gridLayout_4.addWidget(self.sb_number_lines_start_2, 1, 1, 1, 1)
+
+        self.lb_number_ids_step_2 = QLabel(self.gb_functions_2)
+        self.lb_number_ids_step_2.setObjectName(u"lb_number_ids_step_2")
+
+        self.gridLayout_4.addWidget(self.lb_number_ids_step_2, 7, 0, 1, 1)
+
+        self.lb_number_lines_step_2 = QLabel(self.gb_functions_2)
+        self.lb_number_lines_step_2.setObjectName(u"lb_number_lines_step_2")
+
+        self.gridLayout_4.addWidget(self.lb_number_lines_step_2, 2, 0, 1, 1)
+
+        self.ln_functions_5 = QFrame(self.gb_functions_2)
+        self.ln_functions_5.setObjectName(u"ln_functions_5")
+        self.ln_functions_5.setFrameShadow(QFrame.Plain)
+        self.ln_functions_5.setFrameShape(QFrame.HLine)
+
+        self.gridLayout_4.addWidget(self.ln_functions_5, 4, 0, 1, 2)
+
+        self.lb_number_lines_start_2 = QLabel(self.gb_functions_2)
+        self.lb_number_lines_start_2.setObjectName(u"lb_number_lines_start_2")
+
+        self.gridLayout_4.addWidget(self.lb_number_lines_start_2, 1, 0, 1, 1)
+
+        self.lb_number_ids_start_2 = QLabel(self.gb_functions_2)
+        self.lb_number_ids_start_2.setObjectName(u"lb_number_ids_start_2")
+
+        self.gridLayout_4.addWidget(self.lb_number_ids_start_2, 6, 0, 1, 1)
+
+        self.pb_number_ids_2 = QPushButton(self.gb_functions_2)
+        self.pb_number_ids_2.setObjectName(u"pb_number_ids_2")
+
+        self.gridLayout_4.addWidget(self.pb_number_ids_2, 8, 0, 1, 2)
+
+        self.sb_number_ids_start_2 = QSpinBox(self.gb_functions_2)
+        self.sb_number_ids_start_2.setObjectName(u"sb_number_ids_start_2")
+        self.sb_number_ids_start_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.sb_number_ids_start_2.setMaximum(10000)
+        self.sb_number_ids_start_2.setSingleStep(10)
+        self.sb_number_ids_start_2.setValue(100)
+
+        self.gridLayout_4.addWidget(self.sb_number_ids_start_2, 6, 1, 1, 1)
+
+        self.pb_convert_tabs_to_spaces_2 = QPushButton(self.gb_functions_2)
+        self.pb_convert_tabs_to_spaces_2.setObjectName(u"pb_convert_tabs_to_spaces_2")
+
+        self.gridLayout_4.addWidget(self.pb_convert_tabs_to_spaces_2, 13, 0, 1, 2)
+
+        self.lb_tab_width_2 = QLabel(self.gb_functions_2)
+        self.lb_tab_width_2.setObjectName(u"lb_tab_width_2")
+
+        self.gridLayout_4.addWidget(self.lb_tab_width_2, 12, 0, 1, 1)
+
+        self.sb_tab_width_2 = QSpinBox(self.gb_functions_2)
+        self.sb_tab_width_2.setObjectName(u"sb_tab_width_2")
+        self.sb_tab_width_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.sb_tab_width_2.setMinimum(2)
+        self.sb_tab_width_2.setMaximum(8)
+        self.sb_tab_width_2.setSingleStep(2)
+        self.sb_tab_width_2.setValue(4)
+
+        self.gridLayout_4.addWidget(self.sb_tab_width_2, 12, 1, 1, 1)
+
+        self.sb_number_lines_step_2 = QSpinBox(self.gb_functions_2)
+        self.sb_number_lines_step_2.setObjectName(u"sb_number_lines_step_2")
+        self.sb_number_lines_step_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.sb_number_lines_step_2.setMaximum(10000)
+        self.sb_number_lines_step_2.setSingleStep(10)
+        self.sb_number_lines_step_2.setValue(10)
+
+        self.gridLayout_4.addWidget(self.sb_number_lines_step_2, 2, 1, 1, 1)
+
+        self.sb_number_ids_step_2 = QSpinBox(self.gb_functions_2)
+        self.sb_number_ids_step_2.setObjectName(u"sb_number_ids_step_2")
+        self.sb_number_ids_step_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.sb_number_ids_step_2.setMaximum(100)
+        self.sb_number_ids_step_2.setValue(1)
+
+        self.gridLayout_4.addWidget(self.sb_number_ids_step_2, 7, 1, 1, 1)
+
+        self.pb_number_lines_2 = QPushButton(self.gb_functions_2)
+        self.pb_number_lines_2.setObjectName(u"pb_number_lines_2")
+
+        self.gridLayout_4.addWidget(self.pb_number_lines_2, 3, 0, 1, 2)
+
+        self.pb_remove_trailing_whitespace_2 = QPushButton(self.gb_functions_2)
+        self.pb_remove_trailing_whitespace_2.setObjectName(u"pb_remove_trailing_whitespace_2")
+
+        self.gridLayout_4.addWidget(self.pb_remove_trailing_whitespace_2, 15, 0, 1, 2)
+
+        self.ln_functions_6 = QFrame(self.gb_functions_2)
+        self.ln_functions_6.setObjectName(u"ln_functions_6")
+        self.ln_functions_6.setFrameShadow(QFrame.Plain)
+        self.ln_functions_6.setFrameShape(QFrame.HLine)
+
+        self.gridLayout_4.addWidget(self.ln_functions_6, 14, 0, 1, 2)
+
+
+        self.gridLayout_3.addWidget(self.gb_functions_2, 1, 1, 1, 2)
+
+        self.verticalSpacer = QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.gridLayout_3.addItem(self.verticalSpacer, 2, 1, 1, 2)
+
+        self.pte_folder_preview = QPlainTextEdit(self.tab_folder_mode)
+        self.pte_folder_preview.setObjectName(u"pte_folder_preview")
+        self.pte_folder_preview.setFont(font)
+        self.pte_folder_preview.setLineWrapMode(QPlainTextEdit.NoWrap)
+        self.pte_folder_preview.setReadOnly(True)
+        self.pte_folder_preview.setTabStopDistance(40.000000000000000)
+
+        self.gridLayout_3.addWidget(self.pte_folder_preview, 1, 0, 2, 1)
+
+
+        self.verticalLayout_3.addLayout(self.gridLayout_3)
+
+        self.tabWidget.addTab(self.tab_folder_mode, "")
+
+        self.verticalLayout.addWidget(self.tabWidget)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -221,6 +392,9 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(1)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -248,5 +422,26 @@ class Ui_MainWindow(object):
         self.pb_number_lines.setText(QCoreApplication.translate("MainWindow", u"Number Lines", None))
         self.pb_remove_trailing_whitespace.setText(QCoreApplication.translate("MainWindow", u"Remove Trailing Whitespace", None))
         self.pb_save_file_as.setText(QCoreApplication.translate("MainWindow", u"Save File As", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_file_mode), QCoreApplication.translate("MainWindow", u"File Mode", None))
+        self.le_folder_path.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Folder Path", None))
+#if QT_CONFIG(tooltip)
+        self.tb_reload_folder.setToolTip(QCoreApplication.translate("MainWindow", u"Reload Folder", None))
+#endif // QT_CONFIG(tooltip)
+        self.tb_reload_folder.setText(QCoreApplication.translate("MainWindow", u"\u2b6e", None))
+        self.pb_open_folder.setText(QCoreApplication.translate("MainWindow", u"Open Folder", None))
+        self.gb_functions_2.setTitle(QCoreApplication.translate("MainWindow", u"Functions", None))
+        self.sb_number_lines_start_2.setPrefix(QCoreApplication.translate("MainWindow", u"N", None))
+        self.lb_number_ids_step_2.setText(QCoreApplication.translate("MainWindow", u"Step:", None))
+        self.lb_number_lines_step_2.setText(QCoreApplication.translate("MainWindow", u"Step:", None))
+        self.lb_number_lines_start_2.setText(QCoreApplication.translate("MainWindow", u"Start:", None))
+        self.lb_number_ids_start_2.setText(QCoreApplication.translate("MainWindow", u"Start:", None))
+        self.pb_number_ids_2.setText(QCoreApplication.translate("MainWindow", u"Number IDS", None))
+        self.sb_number_ids_start_2.setPrefix(QCoreApplication.translate("MainWindow", u"IDS=", None))
+        self.pb_convert_tabs_to_spaces_2.setText(QCoreApplication.translate("MainWindow", u"Convert Tabs To Spaces", None))
+        self.lb_tab_width_2.setText(QCoreApplication.translate("MainWindow", u"Tab Width", None))
+        self.pb_number_lines_2.setText(QCoreApplication.translate("MainWindow", u"Number Lines", None))
+        self.pb_remove_trailing_whitespace_2.setText(QCoreApplication.translate("MainWindow", u"Remove Trailing Whitespace", None))
+        self.pte_folder_preview.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Folder Preview", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_folder_mode), QCoreApplication.translate("MainWindow", u"Folder Mode", None))
     # retranslateUi
 
