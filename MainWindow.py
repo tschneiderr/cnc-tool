d7ee45e20bf4d015e6b40929d6dabe791520ebbc
Thomas Schneider <ts97@protonmail.com>
2022-06-02 13:12:07 -0700

    Set default tab to file mode

--- MainWindow.py
+++ MainWindow.py
@@ -393,7 +393,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
